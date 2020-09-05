0b28a1c5f7612fdb5d1484d9ae63552b3a0405fb
Serhii Khoma <srghma@gmail.com>
2020-09-05 08:57:44 +0300

    feat: LongLineSmallWidthFirstLineShouldNotBeUnrouped

---
+++ test/snapshots/DodoLongLineSmallWidthFirstLineShouldNotBeUngrouped.purs
@@ -0,0 +1,25 @@
+module DodoLongLineSmallWidthFirstLineShouldNotBeUngrouped where
+
+import Prelude
+
+import Dodo (Doc, foldWithSeparator, indent, plainText, print, text, twoSpaces)
+import Dodo.Common (leadingComma, pursSquares)
+import Effect (Effect)
+import Effect.Class.Console as Console
+
+test1 :: forall a. Doc a
+test1 = do
+  let array = pursSquares <<< foldWithSeparator leadingComma
+  array
+    [ indent $ array
+        [ indent $ array [ indent $ text "0", indent $ text "1" ]
+        , indent $ array [ indent $ text "0", indent $ text "1" ]
+        , indent $ array [ indent $ text "0", indent $ text "1" ]
+        , indent $ array [ indent $ text "0", indent $ text "1" ]
+        ]
+    ]
+
+main :: Effect Unit
+main = do
+  Console.log $ print plainText (twoSpaces { pageWidth = 200 }) test1
+  Console.log $ print plainText (twoSpaces { pageWidth = 20 }) test1
